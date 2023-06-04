bbcdcdfb5a8b4d00fb10e0070a4932847ac0b6ac
felocsss <vlad.shipilov.felocs@gmail.com>
2023-06-04 20:18:59 +0300

    Update Flowers.sql

--- bd/Flowers.sql
+++ bd/Flowers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 31 2023 г., 14:28
+-- Время создания: Июн 04 2023 г., 20:18
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- База данных: `Flowers`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categorys`
+--
+
+CREATE TABLE `categorys` (
+  `id` int NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `categorys`
+--
+
+INSERT INTO `categorys` (`id`, `name`) VALUES
+(1, 'Струнные'),
+(3, 'Клавишные ');
 
 -- --------------------------------------------------------
 
@@ -53,23 +72,21 @@ INSERT INTO `orders` (`id`, `id_user`, `date`, `status`, `comment`, `products_in
 CREATE TABLE `products` (
   `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `about` varchar(100) NOT NULL,
   `price` int NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `country` varchar(50) NOT NULL
+  `description` varchar(1000) NOT NULL,
+  `category` varchar(150) NOT NULL,
+  `country` varchar(150) NOT NULL,
+  `year` int NOT NULL,
+  `image` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `about`, `price`, `image`, `country`) VALUES
-(1, 'петунья', 'красная', 500, '2.jpeg', 'Россия'),
-(2, 'роза', 'розовая', 800, '1.jpeg', 'Германия'),
-(3, 'ромашка', 'белая', 300, '3.jpeg', 'Чехия'),
-(4, 'гвоздика', 'лиловая', 700, '1.jpeg', ''),
-(5, 'сирень', 'фиолетовая', 500, '3.jpeg', ''),
-(6, 'яблоня', 'антоновка', 2000, '2.jpeg', '');
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `category`, `country`, `year`, `image`) VALUES
+(7, 'Гитара', 500, 'Крутая гитара', 'Струнные', 'США', 2002, '1.jpeg'),
+(8, 'Пианино', 56000, 'Крутое пианино', 'Клавишные ', 'Израиль', 2015, '6.jpeg');
 
 -- --------------------------------------------------------
 
@@ -94,13 +111,20 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `surname`, `patronymic`, `login`, `email`, `password`) VALUES
 (1, 'Ася', 'Оствальд', '55', '123', 'asyakynz@gmail.com', '11'),
 (2, 'Ася', 'Оствальд', '55', '123', 'asyakynz@gmail.com', '11'),
-(3, 'Ася', 'Оствальд', '111', '123', 'asyakynz@gmail.com', '80963'),
+(3, 'Ася', 'Оствальд', '11', '123', 'asyakynz@gmail.com', '80963'),
 (4, '1', '1', '1', '1', '1', '1'),
-(5, '1', '1', '1', 'admin', 'ivan@', 'admin22');
+(5, '1', '1', '1', 'admin', 'ivan@', 'admin22'),
+(6, 'Влад', 'Шипилов', 'Алексеевич', 'felocS', 'vlad.shipilov.felocs@gmail.com', '5');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `categorys`
+--
+ALTER TABLE `categorys`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `orders`
@@ -125,6 +149,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `categorys`
+--
+ALTER TABLE `categorys`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
@@ -134,13 +164,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
